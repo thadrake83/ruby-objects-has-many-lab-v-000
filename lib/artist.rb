28bd7f7b5aabb8a3aceaeff849659c0f14615446
Drake Rentrop <drakerentrop@gmail.com>
2019-03-08 00:47:41 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :name, :genre
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -11,10 +12,12 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count +=1
   end
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@song_count +=1
   end
 end
